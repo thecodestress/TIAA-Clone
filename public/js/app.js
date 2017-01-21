c07c2bbaaa3956527be18a39085b363fd614bd42
Ryan <ryan@thecodestress.com>
2017-01-21 16:17:48 -0500

    Commit of the public folder with sesitive information removed

---
+++ public/js/app.js
@@ -0,0 +1,12 @@
+angular.module('visuarchApp',
+    [
+        'ngRoute',
+        'appRoutes',
+        'MainCtrl',
+        'MongoCtrl',
+        'FirebaseCtrl',
+        'CouchCtrl',
+        'TiaaService',
+        'TiaaDirective'
+    ]
+);
